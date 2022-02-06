48315b3d763132d9016f6f943c72ae56fe4c36e1
Shifuu <54868441+ShiyouOu@users.noreply.github.com>
2022-02-06 00:12:30 -0500

    Add Previous Button
    Added Prev Button
    Added some spacing between buttons

--- app/src/main/java/com/sho/geoquizjan2022/QuizActivity.java
+++ app/src/main/java/com/sho/geoquizjan2022/QuizActivity.java
@@ -13,6 +13,7 @@ public class QuizActivity extends AppCompatActivity {
     private Button mTrueButton;
     private Button mFalseButton;
     private Button mNextButton;
+    private Button mPrevButton;
     private TextView mQuestionTextView;
 
     private Question[] mQuestionBank = new Question[] {
@@ -50,6 +51,7 @@ public class QuizActivity extends AppCompatActivity {
         mTrueButton = (Button) findViewById(R.id.true_button);
         mFalseButton = (Button) findViewById(R.id.false_button);
         mNextButton = (Button) findViewById(R.id.next_button);
+        mPrevButton =(Button) findViewById(R.id.prev_button);
 
         mTrueButton.setOnClickListener(new View.OnClickListener(){
             @Override
@@ -69,6 +71,18 @@ public class QuizActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 mCurrentIndex = (mCurrentIndex + 1) % mQuestionBank.length;
+                updateQuestion();
+            }
+        });
+
+        mPrevButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(mCurrentIndex > 0){
+                    mCurrentIndex = (mCurrentIndex - 1) % mQuestionBank.length;
+                }else{
+                    mCurrentIndex = mQuestionBank.length - 1;
+                }
                 updateQuestion();
             }
         });
